e98c4ad43ca8dbfbba49c43e9b52b98758ac7ab1
Tamashi19 <29raul19@gmail.com>
2021-06-23 08:15:59 -0300

    finalização dos exercicios

---
+++ Nome salario estado civil/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Nome_salario_estado_civil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        
+        {
+            int idade;
+             Console.WriteLine("Digite seu nome: ");
+             string nome =Console.ReadLine();
+
+            
+            
+
+              Console.WriteLine("Coloque sua idade ");
+              idade =int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Idade " +idade+ " verificada!!!");
+
+             
+
+             while (idade>150)
+             {
+                Console.WriteLine("!!!ERRO!!! IDADE INVALIDA   Coloque uma idade de 0-150: ");
+                 idade=int.Parse(Console.ReadLine()); 
+
+                 if (idade<=150)
+                 {
+                     Console.WriteLine("Idade " +idade+ " verificada!!!");
+                 }
+
+                 
+             }
+
+                  
+                  
+
+              while (idade<0)
+             {
+                Console.WriteLine("!!!ERRO!!! IDADE INVALIDA   Coloque uma idade de 0-150: ");
+                 idade=int.Parse(Console.ReadLine()); 
+
+                 if (idade<=150)
+                 {
+                     Console.WriteLine("idade " +idade+ " verificada!!!");
+                 }
+
+                 
+             }
+
+             Console.WriteLine("Informe seu salario: ");
+              int salario =int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Idade " +idade+ " verificada!!!");
+
+             
+
+             while (salario<1)
+             {
+                Console.WriteLine("!!!ERRO!!! SALARIO INVALIDO   Coloque uma salario maior que 0: ");
+                 salario=int.Parse(Console.ReadLine()); 
+
+        }
+        
+
+        Console.WriteLine("Qual seu estado civil 's'(solteiro(a)), 'c'(casado(a)), 'v'(viuvo(a)), 'd' (divorciado(a)) : ");
+        string estado=Console.ReadLine();
+
+        
+    }
+    }
+    }
+
--- nome d senha/Program.cs
+++ nome d senha/Program.cs
@@ -6,30 +6,26 @@ namespace nome_d_senha
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite seu nome de usuario");
+            Console.WriteLine("Digite seu nome de usuario: ");
             string login =Console.ReadLine();
-            Console.WriteLine("Coloque uma note de 0-10: ");
+            Console.WriteLine("Coloque sua senha: ");
             string senha =Console.ReadLine();
 
              
 
-             while ()
+             while (login==senha)
              {
-                 Console.WriteLine("Digite seu nome de usuario");
-                 string login =Console.ReadLine();
-                 Console.WriteLine("Coloque uma note de 0-10: ");
-                 string senha =Console.ReadLine();
-               
-                 if ()
-                 {
-                     
-                 }
 
-                 
-                 
-
+                 Console.WriteLine("ERRO seu nome de usuario não pode ser igual a sehnha, tente novamente ");
+                 Console.WriteLine("Digite seu nome de usuario: ");
+                  login =Console.ReadLine();
+                 Console.WriteLine("Coloque sua senha: ");
+                  senha =Console.ReadLine();
              }
 
+             
+                 Console.WriteLine("Login feito com sucesso!!! ");
+             
         }
     }
 }
